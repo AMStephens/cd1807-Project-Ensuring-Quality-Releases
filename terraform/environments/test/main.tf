111b28bfd7ed8ed18e61b529f080ae1e733344d2
Alice Stephens <71175451+AMStephens@users.noreply.github.com>
2023-07-15 14:59:27 +0100

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -54,10 +54,10 @@ module "publicip" {
 }
 module "vm" {
   source           = "../../modules/vm"
-  location         = "${var.location}"
-  application_type = "${var.application_type}"
-  resource_group   = "${module.resource_group.resource_group_name}"
-  resource_type    = "VM"
-  subnet_id        = module.network.subnet_id_test
-  pip_id           = module.publicip.public_ip_address_id
+  #location         = "${var.location}"
+  #application_type = "${var.application_type}"
+  #resource_group   = "${module.resource_group.resource_group_name}"
+  #resource_type    = "VM"
+  #subnet_id        = module.network.subnet_id_test
+  #pip_id           = module.publicip.public_ip_address_id
 }
