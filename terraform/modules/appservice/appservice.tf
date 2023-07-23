a6104a27bf186126e99eca84092d6d923c022bd7
Alice Stephens <71175451+AMStephens@users.noreply.github.com>
2023-07-23 15:09:20 +0100

    Update appservice.tf

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -20,3 +20,29 @@ resource "azurerm_linux_web_app" "test" {
     always_on = false
   }
 }
+
+data "azurerm_monitor_action_group" "existing" {
+  name                = "Alices_Alerts"
+  resource_group_name = "alices_resource_group"
+}
+
+resource "azurerm_monitor_metric_alert" "main" {
+  name                = "alices-metric-alert"
+  resource_group_name = "alices_resource_group"
+  scopes              = [azurerm_linux_web_app.test.id]
+  target_resource_type = "Microsoft.Web/sites"
+  description         = "Metric alert"
+  severity            = 2
+
+  criteria {
+    metric_namespace = "Microsoft.Web/sites"
+    metric_name      = "FailedRequests"
+    aggregation      = "Total"
+    operator         = "GreaterThan"
+    threshold        = 0
+  }
+
+  action {
+    action_group_id = data.azurerm_monitor_action_group.existing.id
+  }
+}
